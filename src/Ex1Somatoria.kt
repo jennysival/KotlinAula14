3fee26e788a6822d74105306115936e12cd77c42
jennysival <jeniffersival@gmail.com>
2022-04-18 14:40:54 -0300

    Criar a primeiro função para receber o número do usuário

--- src/Ex1Somatoria.kt
+++ src/Ex1Somatoria.kt
@@ -1,7 +1,14 @@
 //Faça um algoritmo que contenha um método que receba um número do
 //usuário e realize a somatória de todos os números no intervalo de 1 até o
-//número digitado pelo usuário. Exiba o resultado final da somatória.
+//número digitado pelo usuário. Exiba o resultado da somatória.
 
 fun main() {
 
+}
+
+fun receberNumero(): Int{
+    println("Digite um número:")
+    val numeroDigitado = readln().toInt()
+
+    return numeroDigitado
 }
