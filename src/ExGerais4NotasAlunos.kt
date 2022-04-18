c9cffd0fa31f2d847b459e26a6de0f6e47b84232
jennysival <jeniffersival@gmail.com>
2022-04-18 18:23:10 -0300

    Resolução do exercício 4, calculando a média de notas de alunos usando 4 funções

--- src/ExGerais4NotasAlunos.kt
+++ src/ExGerais4NotasAlunos.kt
@@ -1,27 +1,54 @@
+import kotlin.system.exitProcess
+
 //Escrever um programa que leia o nome de N alunos e as notas das
 //três provas que ele obteve no semestre. No final informar o nome do
 //aluno e a sua média (aritmética)
 //Obs: use vetores para a resolução do exercício
 
 fun main() {
-
+    cadastroAluno()
 }
 
-fun cadastroAlunos(){
-    println("Quantos alunos deseja cadastrar?")
-    val qtdAlunos = readln().toInt()
+fun cadastroAluno(){
+    println("Digite o nome do(a/e) aluno(a/e):")
+    val NomeAluno = readln()
 
-    val nomeAlunos = arrayOfNulls<String>(qtdAlunos)
-
-    for(i: Int in nomeAlunos.indices){
-        println("Digite o nome do ${i+1}º aluno:")
-        nomeAlunos[i] = readln()
+    val notas = IntArray(3)
+    for (i: Int in notas.indices){
+        println("Digite a ${i+1}ª nota de $NomeAluno:")
+        notas[i] = readln().toInt()
     }
 
-    cadastroNotas(qtdAlunos, nomeAlunos)
+    calculoMedia(NomeAluno, notas)
 }
 
-fun cadastroNotas(qtdAlunos: Int, nomeAlunos: Array<String?>) {
+fun calculoMedia(NomeAluno: String, notas: IntArray) {
+    var soma=0
 
+    for(i: Int in notas.indices){
+        soma = soma+notas[i]
+    }
+
+    val media = soma/3
+
+    println("A média de notas de $NomeAluno foi: $media")
+
+    OpcAdicionarNovoAluno()
+}
+
+fun OpcAdicionarNovoAluno(){
+    println("-----------------------------------------")
+    println("Deseja adicionar um novo(a/e) aluno(a/e)?")
+    println("1) SIM")
+    println("2) NÃO")
+    println("-----------------------------------------")
+    println("Digite apenas o número da opção escolhida:")
+    val opc = readln().toInt()
+
+    when(opc){
+        1 -> cadastroAluno()
+        2 -> exitProcess(0)
+        else -> println("Seleção Inválida")
+    }
 }
 
